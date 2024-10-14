926f2e80254c16af4689b16086d7881f2935972b
shanuka_2242 <shanukapayoe@gmail.com>
2024-10-15 02:05:55 +0530

    Changed status bar color

--- PawPrint/Platforms/Android/MainActivity.cs
+++ PawPrint/Platforms/Android/MainActivity.cs
@@ -1,10 +1,16 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
 
 namespace PawPrint
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#00BF63"));
+        }
     }
 }
