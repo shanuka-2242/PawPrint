cf921310fa9702c92f48e87f2a78fe8d9a032e0b
shanuka_2242 <shanukapayoe@gmail.com>
2024-10-26 17:10:32 +0530

    code changes

--- PawPrint/Models/Dog.cs
+++ PawPrint/Models/Dog.cs
@@ -5,5 +5,5 @@ public class Dog
     public int EntryID { get; set; }
     public string Name { get; set; }
     public string Breed { get; set; }
-    public int Age { get; set; }
+    public string Age { get; set; }
 }
